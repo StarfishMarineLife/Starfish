d52972ff7355774fb92dd1c6648b25d892de8e51
Paul Fears <paulrfears@gmail.com>
2021-07-14 20:58:27 +0000

    fixed spelling

---
+++ walletconnect.js
@@ -0,0 +1,19 @@
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        // Mikko's test key - don't copy as your mileage may vary
+        rpc: {
+          56: "https://bsc-dataseed.binance.org/"
+        },
+      }
+    },
+
+    fortmatic: {
+      package: Fortmatic,
+      options: {
+        // Mikko's TESTNET api key
+        key: "pk_test_391E26A3B43A3350"
+      }
+    }
+  };
